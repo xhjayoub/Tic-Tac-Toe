aa00430fdaf87a6c96632f222023836ae55ec472
Ayoub hj <medayoubhajamor197@gmail.com>
2024-09-15 15:05:30 +0100

    Update insertPlayerInput Doesn't accept position that is not undefined

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function gameBoard() {
     let gameBoardArr = [[],[],[]];
     let player = "X";
     const insertPlayerInput = function(posArr) {
-        gameBoardArr[posArr[0]][posArr[1]] = player;
+        if (gameBoardArr[posArr[0]][posArr[1]] === undefined) {
+            gameBoardArr[posArr[0]][posArr[1]] = player;
+        } else {
+            return false;
+        }
         if (player === "X") {
             player = "O";
         } else {
