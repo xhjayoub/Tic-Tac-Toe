d01f5cc87ef1bf92c89482f5d7601b770650682b
Ayoub hj <medayoubhajamor197@gmail.com>
2024-09-15 15:32:34 +0100

    Add checkWinner()

--- script.js
+++ script.js
@@ -21,6 +21,23 @@ function gameBoard() {
     const checkLine = (index) => { return (gameBoardArr[index][0] === gameBoardArr[index][1]) && (gameBoardArr[index][0] === gameBoardArr[index][2])};
     const checkDiag1 = () => { return (gameBoardArr[0][0] === gameBoardArr[1][1]) && (gameBoardArr[0][0] === gameBoardArr[2][2])};
     const checkDiag2 = () => { return (gameBoardArr[0][2] === gameBoardArr[1][1]) && (gameBoardArr[0][2] === gameBoardArr[2][0])};
+    const checkWinner = function() {
+        if (checkDiag1()) {
+            return gameBoardArr[0][0];
+        }
+        if (checkDiag2()) {
+            return gameBoardArr[2][0];
+        }
+        if (checkLine(0)) {
+            return gameBoardArr[0][0];
+        }
+        if (checkLine(1)) {
+            return gameBoardArr[1][0];
+        }
+        if (checkLine(2)) {
+            return gameBoardArr[2][0];
+        }
+    };
     return { insertPlayerInput, displayGameBoard };
 }
 const game = gameBoard();
